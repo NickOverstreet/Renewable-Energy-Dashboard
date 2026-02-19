4db30876b65b5dad9e2e38356c5f4a5ced0708c9
Cole Hinkes <cbhinkes@Coles-MacBook-Pro.local>
2026-02-19 12:09:09 -0500

    Made rolling capacity changes

--- dashboard-scripts/get_rolling_capacity_factors.php
+++ dashboard-scripts/get_rolling_capacity_factors.php
@@ -10,7 +10,11 @@ $dbh = new PDO(
 
 // Prepare a query to fetch the most recent rolling capacity factors
 $statement = $dbh->prepare(
-    "SELECT solar_capacity_factor_7d, hydro_capacity_factor_7d
+    "SELECT solar_total_capacity_factor_7d, 
+                   solar_fixed_capacity_factor_7d, 
+                   solar_dual_capacity_factor_7d, 
+                   hydro_capacity_factor_7d, 
+                   wind_capacity_factor_7d
      FROM capacity_factor_history
      ORDER BY date_time DESC
      LIMIT 1"
--- dashboard-scripts/rollingCapacityFactors.js
+++ dashboard-scripts/rollingCapacityFactors.js
@@ -1,17 +1,29 @@
 function updateRollingCapacityFactors() {
     // Make an HTTP request to get_rolling_capacity_factors.php
-    fetch("get_rolling_capacity_factors.php") 
+    fetch("get_rolling_capacity_factors.php")
         // Read the response body and parse it as JSON
         .then(res => res.json())
         // Use the parsed JSON data
         .then(data => {
-            if (data.solar_capacity_factor_7d !== null) {
-                document.getElementById('solarCF7d').textContent =
-                    data.solar_capacity_factor_7d.toFixed(2) + '%';
+            if (data.solar_total_capacity_factor_7d !== null) {
+                document.getElementById('solartotalCF7d').textContent =
+                    data.solar_total_capacity_factor_7d.toFixed(2) + '%';
+            }
+            if (data.solar_fixed_capacity_factor_7d !== null) {
+                document.getElementById('solarfixedCF7d').textContent =
+                    data.solar_fixed_capacity_factor_7d.toFixed(2) + '%';
+            }
+            if (data.solar_dual_capacity_factor_7d !== null) {
+                document.getElementById('solardualCF7d').textContent =
+                    data.solar_dual_capacity_factor_7d.toFixed(2) + '%';
             }
             if (data.hydro_capacity_factor_7d !== null) {
                 document.getElementById('hydroCF7d').textContent =
                     data.hydro_capacity_factor_7d.toFixed(2) + '%';
+            }
+            if (data.wind_capacity_factor_7d !== null) {
+                document.getElementById('windCF7d').textContent =
+                    data.wind_capacity_factor_7d.toFixed(2) + '%';
             }
         })
         .catch(err => console.error('CF fetch error:', err));
